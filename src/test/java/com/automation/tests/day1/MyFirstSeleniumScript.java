c83bf2c4bc8100afb641026e0f701b2be012fc14
zeeyoda <ziyoda2000@yahoo.com>
2020-03-02 10:42:27 -0500

    first commit

---
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -0,0 +1,16 @@
+package com.automation.tests.day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstSeleniumScript {
+    public static void main(String[] args) {
+        //set up chromedriver
+        WebDriverManager.chromedriver().setup();
+        //create chromedriverobject
+        ChromeDriver driver = new ChromeDriver();
+        //open some website
+        driver.get("http://google.com");
+    }
+}
