edee83a53452b775f1a0e251b834945af99578a5
zeeyoda <ziyoda2000@yahoo.com>
2020-03-02 14:49:06 -0500

    Created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         //to start selenium script we need:
         //setup webdriver (browser driver) and create webdriver object
 
@@ -16,6 +16,23 @@ public class BasicNavigation {
 //        RemoteWebDriver driver1 = new FirefoxDriver();
 //        RemoteWebDriver driver2 = new SafariDriver();
         //in selenium, everything starts from webdriver interface
-        driver.get("http://google.com");// is used to open website. now I will push to github
+        //ChromeDriver extends RemoteWebDriver --- implements WebDriver
+        driver.get("http://google.com");// is used to open website.
+
+        Thread.sleep(3000);//for demo wait 3 seconds
+
+        //method that return page title
+        //you can also see it as teb name, in the browser
+        String title = driver.getTitle();// returns <title>Some title</title> test
+        String expectedTitle = "Google";
+
+        System.out.println("Title is ..."+title);
+        if(expectedTitle.equals(title)) {
+            System.out.println("Test Passed!");
+        }else{
+            System.out.println("Test Failed!");
+        }
+        driver.close();//to close browser
+        //browser cannot close itself
     }
 }
