9db8dc69eebe8d4a1d754ceb99bce43247a79e30
zeeyoda <ziyoda2000@yahoo.com>
2020-03-03 10:14:51 -0500

    Created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -18,8 +18,9 @@ public class BasicNavigation {
         //in selenium, everything starts from webdriver interface
         //ChromeDriver extends RemoteWebDriver --- implements WebDriver
         driver.get("http://google.com");// is used to open website.
+        driver.manage().window().maximize();
 
-        Thread.sleep(3000);//for demo wait 3 seconds
+        Thread.sleep(2000);//for demo wait 3 seconds
 
         //method that return page title
         //you can also see it as teb name, in the browser
@@ -32,7 +33,42 @@ public class BasicNavigation {
         }else{
             System.out.println("Test Failed!");
         }
+        //navigate to another page within same window
+        //driver.navigate().to() is the sam as driver.navigate.get()
+        driver.navigate().to("http:amazon.com");
+        Thread.sleep(2000);
+        if(driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("Test passed");
+        }else{
+            System.out.println("Test failed");
+        }
+        //comeback to google
+        driver.navigate().back();
+        Thread.sleep(2000);
+
+        verifyEquals(driver.getTitle(), "Google");
+
+        //move forward in the browser history
+
+        driver.navigate().forward();
+        Thread.sleep(2000);
+        System.out.println("Title "+driver.getTitle());
+        //returns the title of the page
+
+        //getting current url
+        System.out.println("URL: "+driver.getCurrentUrl());
+
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+
         driver.close();//to close browser
         //browser cannot close itself
+    }
+    public static void verifyEquals(String str1, String str2){
+        if(str1.equals(str2)){
+            System.out.println("Test passed");
+        }else{
+            System.out.println("Test failed");
+        }
     }
 }
