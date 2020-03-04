1875a392f1583a80e63aac362d34b82a59df3d16
zeeyoda <ziyoda2000@yahoo.com>
2020-03-03 19:46:59 -0500

    JumpToNewWindow

---
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -0,0 +1,16 @@
+package com.automation.tests.day3;
+
+import com.automation.utilities.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementsPractice {
+    public static void main(String[] args) {
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        driver.quit();
+    }
+}
